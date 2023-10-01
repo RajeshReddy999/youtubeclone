80063c931f20e8d7d9ae0b59c66b5f258621e13b
rajesh reddy <rajeshreddysingireddy97@gmail.com>
2023-10-01 18:17:07 +0530

    YoutubeClone from JSmasteryYoutube Channel

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+import axios, { request } from "axios";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+const options = {
+	params: {
+		maxResults: "50",
+	},
+	headers: {
+		"X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+		"X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+	},
+};
+
+export const fetchFromAPI = async (url) => {
+	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+	return data;
+};
+
+// try {
+// 	const response = await request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
